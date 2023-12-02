53852f7f9134659249f2bddd9f5bbb6b4f14cdf2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-02 08:29:30 -0500

    create dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+interface Props {}
+
+const page: FC<Props> = (props): JSX.Element => {
+  return (
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default page;

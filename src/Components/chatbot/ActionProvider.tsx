609a0208c8a9519059970790053ac05bfe8ce6e6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-02 06:41:10 -0500

    create action provider. not yet working

--- src/Components/chatbot/ActionProvider.tsx
+++ src/Components/chatbot/ActionProvider.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
+import OpenAI from 'openai';
 
-// Define the props interface
 interface ActionProviderProps {
-  createChatBotMessage: (message: string, options?: object) => void; // Assuming this is the signature of createChatBotMessage
-  setState: React.Dispatch<React.SetStateAction<any>>; // Adjust according to your state structure
+  createChatBotMessage: (message: string, options?: object) => React.ReactNode;
+  setState: React.Dispatch<React.SetStateAction<any>>;
   children: React.ReactNode;
 }
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY as string, // Type assertion for the API key
+});
 
 const ActionProvider: React.FC<ActionProviderProps> = ({
   createChatBotMessage,
   setState,
   children,
 }) => {
-  // Implement your actions here
+  const getResponseFromOpenAI = async (userInput: string) => {
+    try {
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: userInput }],
+        model: 'gpt-3.5-turbo',
+      });
+
+      // Check if content is not null before using it
+      const content = chatCompletion.choices[0].message.content;
+      const aiMessageText = content ? content : "Sorry, I didn't get that.";
+      const aiMessage = createChatBotMessage(aiMessageText);
+      updateChatbotState(aiMessage);
+    } catch (error) {
+      const errorMessage = createChatBotMessage(
+        'An error occurred while contacting the AI service.',
+      );
+      updateChatbotState(errorMessage);
+    }
+  };
+
+
+  const updateChatbotState = (message: React.ReactNode) => {
+    setState((prevState: any) => ({
+      ...prevState,
+      messages: [...prevState.messages, message],
+    }));
+  };
 
   return (
     <div>
       {React.Children.map(children, (child) => {
         return React.cloneElement(child as React.ReactElement, {
-          actions: {}, // You can pass your actions here
+          actions: { getResponseFromOpenAI },
         });
       })}
     </div>
